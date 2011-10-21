8c2ea35d1317ca8d364e3e78a69dde85552a0c80
Anthony Garand <anthonygarand@gmail.com>
2011-10-21 16:05:50 -0400

    Responsive adjustments

--- scripts/core.js
+++ scripts/core.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("h1").fitText(1.5,{maxFontSize:'42px'});
+  $("h1").fitText(2,{maxFontSize:'42px'});
   $("h2").fitText(.5,{maxFontSize:'28px'});
   $("nav ul li a").fitText(.8,{maxFontSize:'15px'});
 });
