cf013e4c1bbac34cbedf3aeb8bef507679a9979b
Anthony Garand <garand@garand.me>
2015-07-27 11:14:38 -0400

    Add GoSquared tracking

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 try{Typekit.load();}catch(e){}
-  
+
 $(function(){
 
   _500px.init({
@@ -19,3 +19,10 @@ $(function(){
 
   });
 });
+
+!function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+insertBefore(d,q)}(window,document,'script','_gs');
+
+_gs('GSN-675904-Z');
