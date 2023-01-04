77f6cfe70e949b31630be3c25412bf33966a9932
Anthony Garand <garand@garand.me>
2023-01-04 09:47:12 -0500

    feat: add gagu.es tracking

--- app/root.tsx
+++ app/root.tsx
@@ -35,6 +35,25 @@ export default function Component() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+  var _gauges = _gauges || [];
+  (function() {
+    var t   = document.createElement('script');
+    t.type  = 'text/javascript';
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '4d5aa2a95ffe1f587b000001');
+    t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
+    t.src = 'https://d2fuc4clr7gvcn.cloudfront.net/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+  })();
+        `,
+          }}
+        />
       </body>
     </html>
   );
