887f2c575a2d91c36116419205911d0452265d6c
Anthony Garand <garand@garand.me>
2023-10-26 12:43:28 -0400

    chore: upgrade packages

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,16 @@
+import { createRoutesFromFolders } from "@remix-run/v1-route-convention";
+
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
+export default {
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  // serverBuildPath: "build/index.js",
+  routes(defineRoutes) {
+    // uses the v1 convention, works in v1.15+ and v2
+    return createRoutesFromFolders(defineRoutes, {
+      ignoredFilePatterns: ["**/.*"],
+    });
+  },
 };
