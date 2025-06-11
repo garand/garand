401f78166c34c096c18de3b099eacc08c6086438
Anthony Garand <garand@garand.me>
2025-06-10 22:34:27 -0400

    devinxi

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [tsConfigPaths(), tailwindcss(), tanstackStart()],
+});
